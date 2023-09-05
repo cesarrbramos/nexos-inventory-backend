4bd6ae66cd62b1266da8896536c8f5a0df16d6ae
cesarbramos <cesarrbramos@gmail.com>
2023-09-04 21:52:55 -0500

    Initial commit

---
+++ src/main/java/com/nexos/inventory/NexosInventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexos.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexosInventoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexosInventoryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexos/inventory/NexosInventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexos.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexosInventoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
